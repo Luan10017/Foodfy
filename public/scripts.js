a356e7499787833c92c12b7031aacc052551ffca
Luan Andrade <luan10017@hotmail.com>
2020-05-28 21:27:56 -0300

    Trabalhando na função esconde/mostra do detalhes da receita

--- public/scripts.js
+++ public/scripts.js
@@ -7,3 +7,15 @@ for (let i = 0;cards.length; i++){
     })
 }
 
+const ingrediente = document.querySelector('.ingredientes')
+const showIng = document.querySelector('.show_ing')
+
+showIng.addEventListener('click', function() {
+    if (showIng.innerHTML == 'Esconder') {
+        ingrediente.classList.add('active')
+        showIng.innerHTML = 'Mostrar'
+    }else {
+        ingrediente.classList.remove('active')
+        showIng.innerHTML = 'Esconder'
+    }
+})
