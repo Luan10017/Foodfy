b269d3985ac290b9b1a106d2b4047fcfd3ca5519
Luan Andrade <luan10017@hotmail.com>
2020-08-06 21:34:53 -0300

    Adicionei a página edit e corrigi alguns estilos

--- routes.js
+++ routes.js
@@ -35,7 +35,6 @@ routes.get('/admin/recipes', function (req, res) {
 routes.get("/admin/recipes/create", function (req, res) {
     return res.render('admin/create')
 })
-//recipes.create); // Mostrar formulário de nova receita
 
 routes.get("/admin/recipes/:index", function (req, res) {
     const index = req.params.index
@@ -47,14 +46,13 @@ routes.get("/admin/recipes/:index", function (req, res) {
 
 })
 
+routes.get("/admin/recipes/:id/edit", function (req, res) {
+    return res.render('admin/edit')
+})
 
-//recipes.show); // Exibir detalhes de uma receita
+/*recipes.edit); // Mostrar formulário de edição de receita*/
 
-
-
-/* routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
-
-routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+/*routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 routes.put("/admin/recipes", recipes.put); // Editar uma receita
 routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita */
 
