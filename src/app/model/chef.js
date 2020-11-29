ccd038a33f2bd2a4cc1e3bc0558901d73f113945
Luan10017 <32424296+Luan10017@users.noreply.github.com>
2020-11-29 11:20:31 -0300

    Refatorei para async await

--- src/app/model/chef.js
+++ src/app/model/chef.js
@@ -3,17 +3,13 @@ const { date } = require('../../lib/utils')
 
 
 module.exports = {
-    chefs(callback) {
-        db.query(`
+    chefs() {
+        return db.query(`
         SELECT chefs.*, count(recipes) AS total_recipes
         FROM chefs
         LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
         GROUP BY chefs.id
-        `, function(err, results){
-            if(err) throw `Database Error! ${err}`
-
-            callback(results.rows)
-        })
+        `)
     },
     find(id) {
         const query = `
@@ -86,6 +82,12 @@ module.exports = {
 
             return callback()
         })
+    },
+    filesId(id) {
+        return db.query(`SELECT * FROM chef_files WHERE chef_id = $1`, [id])
+    },
+    files(id) {
+        return db.query(`SELECT * FROM files WHERE id = $1`, [id])
     }
     
 }
