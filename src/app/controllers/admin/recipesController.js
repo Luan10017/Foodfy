7f204a21e0b950c742fe9991603ea44eb2c4252f
Luan Andrade <luan10017@hotmail.com>
2020-12-08 21:42:25 -0300

    put e delete files

--- src/app/controllers/admin/recipesController.js
+++ src/app/controllers/admin/recipesController.js
@@ -89,11 +89,52 @@ module.exports = {
 
         return res.redirect(`/admin/recipes/${recipeId}`)
     },
-    put(req, res) {
+    async put(req, res) {
         const keys = Object.keys(req.body)
         for (key of keys) {
-            if (req.body[key] == "" && req.body[key]!= req.body.chef && req.body[key]!= req.body.information)
+            if (req.body[key] == "" && key !="removed_files" && req.body[key]!= req.body.chef && req.body[key]!= req.body.information)
                 return res.send('Please, fill all fields.')
+        }
+          
+        //Para apagar files vai ser preciso apagar link no recipe_files e depois na tabela file
+
+
+        if (req.files.length !=0) {
+            const newFilesPromise = req.files.map(file =>
+                File.create(req.body))
+            await Promise.all(newFilesPromise)
+        }
+
+        /* 
+         if (req.files.length == 0)
+            return res.send('Please, send at least one image.')
+
+        let results = await Recipes.create(req.body)
+        const recipeId = results.rows[0].id
+
+        const filesPromise = req.files.map(file => File.create({...file}))
+        const fileResults = await Promise.all(filesPromise)
+        
+        const recipeFilesPromises = fileResults.map(file => {
+            const fileId = file.rows[0].id
+
+            File.createRecipeFiles(fileId, recipeId)
+        })
+
+        await Promise.all(recipeFilesPromises)
+
+        return res.redirect(`/admin/recipes/${recipeId}`)
+        
+        */
+
+        if (req.body.removed_files) {
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
         }
 
         Recipes.update(req.body, function() {
--- src/app/model/File.js
+++ src/app/model/File.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const fs = require('fs')
 
 module.exports = {
     create({filename, path}) {
@@ -55,5 +56,20 @@ module.exports = {
         } catch (err) {
             throw new Error(err)
         }
+    },
+    async delete(id) {
+        try {
+            const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+            const file = result.rows[0]
+    
+            fs.unlinkSync(file.path)
+
+            return db.query(`
+            DELETE FROM files WHERE id = $1
+        `, [id])
+        }catch(err){
+            console.error(err)
+        }
+        
     }
 } 
