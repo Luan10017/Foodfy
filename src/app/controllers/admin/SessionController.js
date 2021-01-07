b7128b7bef305cc26a77fb1500e82c378a2de875
Luan <luan10017@hotmail.com>
2021-01-07 16:10:14 -0300

    Adicionei renders forgot e reset form

--- src/app/controllers/admin/SessionController.js
+++ src/app/controllers/admin/SessionController.js
@@ -3,7 +3,13 @@
 
 module.exports = {
     loginForm(req, res) {
+        return res.render("admin/session/login")
+    },
+    forgotForm(req, res) {
         return res.render("admin/session/forgot-password")
+    },
+    resetForm(req, res) {
+        return res.render("admin/session/password-reset")
     }
 }
 
