349f440b9f8c4a51c37bd68c90307eadf176c0af
Luan <luan10017@hotmail.com>
2021-01-07 21:23:02 -0300

    Adicionei post

--- src/app/controllers/admin/UserController.js
+++ src/app/controllers/admin/UserController.js
@@ -1,5 +1,12 @@
 module.exports = {
     list(req, res) {
         return res.render("admin/user/users-list")
+    },
+    async post(req, res) {
+        const userId = await User.create(req.body)
+
+        req.session.userId = userId
+
+        return res.redirect('/users')
     }
 }
