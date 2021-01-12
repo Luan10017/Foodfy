22d0c17fa694174eb2e411e34b63b3733fe87586
Luan <luan10017@hotmail.com>
2021-01-11 21:20:49 -0300

    Adicionei login e logout

--- src/app/controllers/admin/SessionController.js
+++ src/app/controllers/admin/SessionController.js
@@ -5,6 +5,14 @@ module.exports = {
     loginForm(req, res) {
         return res.render("admin/session/login")
     },
+    login(req, res){
+        req.session.userId = req.user.id
+        return res.redirect("/admin/users")
+    },
+    logout(req, res) {
+        req.session.destroy()
+        return res.redirect("/")
+    },
     forgotForm(req, res) {
         return res.render("admin/session/forgot-password")
     },
