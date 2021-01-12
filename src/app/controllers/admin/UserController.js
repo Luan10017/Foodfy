a1cb0edfa445a567e60620cb95328e6c7fb0b48d
Luan <luan10017@hotmail.com>
2021-01-11 21:22:04 -0300

    Adicionei token e session.userId

--- src/app/controllers/admin/UserController.js
+++ src/app/controllers/admin/UserController.js
@@ -1,5 +1,6 @@
 const User = require("../../model/User")
 const mailer = require('../../../lib/mailer')
+const crypto = require('crypto')
 
 module.exports = {
     list(req, res) {
@@ -12,29 +13,40 @@ module.exports = {
 
 
         return res.render("admin/user/edit")
-    },
-    
+    },  
     async post(req, res) {
-        const randomPassword = Math.floor(Math.random() * 10000) + ""
+        try{
+            const randomPassword = Math.floor(Math.random() * 10000) + ""
+            const token = crypto.randomBytes(20).toString("hex")
+
+            const userId = await User.create(req.body, randomPassword, token)
+            req.session.userId = userId
+
+
+            await mailer.sendMail({
+                to: req.body.email,
+                from: 'no-reply@lanchstore.com.br',
+                subject: 'Confirmação de conta.',
+                html: `<h2>Parabéns sua conta foodfy foi criada com sucesso.</h2>
+                <p>Sua senha temporaria é ${randomPassword}</p>
+                <p>
+                    <a href="http://localhost:3000/admin/users/password-reset?token=${token}" target="blank">
+                        CRIAR NOVA SENHA
+                    </a>
+                </p>
+                `
+            })
+            //success não funciona com redirect?
+            return res.render('admin/user/users-list', {
+                success: "Cadastro realizado com sucesso!"
+            })
+
+        }catch(err) {
+            console.error(err)
+            return res.render("admin/user/register", {
+                error: "Erro inesperado, tente novamente"
+            })
+        }
         
-        const userId = await User.create(req.body, randomPassword)
-       // req.session.userId = userId
-
-
-       await mailer.sendMail({
-            to: req.body.email,
-            from: 'no-reply@lanchstore.com.br',
-            subject: 'Confirmação de conta.',
-            html: `<h2>Parabéns sua conta foodfy foi criada com sucesso.</h2>
-            <p>Sua senha temporaria é ${randomPassword}</p>
-            <p>
-                <a href="http://localhost:3000/users/" target="blank">
-                    CRIAR NOVA SENHA
-                </a>
-            </p>
-            `
-        })
-
-       return res.redirect('/admin/users/')
     }
 }
