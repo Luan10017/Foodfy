c517b6dbc60ebd1919f6697cd7f84cc605c5dd96
Luan <luan10017@hotmail.com>
2021-01-13 19:49:00 -0300

    Adiceionei middleware que só permite usuários logados

--- src/routes/admin/recipes.js
+++ src/routes/admin/recipes.js
@@ -3,12 +3,16 @@ const routes = express.Router()
 const recipesAdminController = require('../../app/controllers/admin/recipesController')
 const multer = require('../../app/middlewares/multer')
 
+const { onlyUsers } = require('../../app/middlewares/session')
+
+
 routes.get('/', recipesAdminController.index)
-routes.get("/create", recipesAdminController.create)
+routes.get("/create", onlyUsers, recipesAdminController.create)
 routes.get("/:index", recipesAdminController.show)
-routes.get("/:index/edit", recipesAdminController.edit)
-routes.post("/",  multer.array("photos",5), recipesAdminController.post) // Cadastrar nova receita
-routes.put("/",  multer.array("photos",5), recipesAdminController.put); // Editar uma receita
-routes.delete("/", recipesAdminController.delete); // Deletar uma receita 
+routes.get("/:index/edit", onlyUsers, recipesAdminController.edit)
+
+routes.post("/", onlyUsers, multer.array("photos",5), recipesAdminController.post) // Cadastrar nova receita
+routes.put("/", onlyUsers, multer.array("photos",5), recipesAdminController.put); // Editar uma receita
+routes.delete("/", onlyUsers, recipesAdminController.delete); // Deletar uma receita 
 
 module.exports = routes
