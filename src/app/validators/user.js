4b8487d7690edb54c52c3cb60aa37081c88beeec
Luan <luan10017@hotmail.com>
2021-01-17 16:11:58 -0300

    adicionei metodo update

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,4 +1,5 @@
 const User = require("../model/User")
+const { compare } = require('bcryptjs')
 
 function checkAllFields(body) {
     const keys = Object.keys(body)
@@ -33,7 +34,33 @@ async function post(req, res, next) {
 
     next()
 }
+async function update (req, res, next) {
+        //validar se todos os campos estão preenchidos
+        const fillAllFields = checkAllFields(req.body)
+        if(fillAllFields){
+            return res.render('admin/user/index', fillAllFields)
+        }
+        //validar se senha bate 
+        const { password } = req.body
+        const id = req.session.userId
+
+        if (!password) return res.render('admin/user/index', {
+            user: req.body,
+            error: 'Coloque sua senha para atualizar seu cadastro'
+        })
+
+        const user = await User.findOne({ where: {id} })
+
+        const passed = await compare(password, user.password)
+
+        if(!passed) return res.render('admin/user/index', {
+            user: req.body,
+            error: 'Senha incorreta.'
+        })
+    next()
+}
 
 module.exports = {
-    post
+    post,
+    update
 }
