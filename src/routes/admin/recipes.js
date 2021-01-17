9037e8024e1ec13daf1218ac51bd66a9af62e028
Luan <luan10017@hotmail.com>
2021-01-17 16:14:53 -0300

    Retirei onlyUsers dessa parte e coloquei na rota global

--- src/routes/admin/recipes.js
+++ src/routes/admin/recipes.js
@@ -3,16 +3,14 @@ const routes = express.Router()
 const recipesAdminController = require('../../app/controllers/admin/recipesController')
 const multer = require('../../app/middlewares/multer')
 
-const { onlyUsers } = require('../../app/middlewares/session')
-
 
 routes.get('/', recipesAdminController.index)
-routes.get("/create", onlyUsers, recipesAdminController.create)
+routes.get("/create", recipesAdminController.create)
 routes.get("/:index", recipesAdminController.show)
-routes.get("/:index/edit", onlyUsers, recipesAdminController.edit)
+routes.get("/:index/edit", recipesAdminController.edit)
 
-routes.post("/", onlyUsers, multer.array("photos",5), recipesAdminController.post) // Cadastrar nova receita
-routes.put("/", onlyUsers, multer.array("photos",5), recipesAdminController.put); // Editar uma receita
-routes.delete("/", onlyUsers, recipesAdminController.delete); // Deletar uma receita 
+routes.post("/", multer.array("photos",5), recipesAdminController.post) // Cadastrar nova receita
+routes.put("/", multer.array("photos",5), recipesAdminController.put); // Editar uma receita
+routes.delete("/", recipesAdminController.delete); // Deletar uma receita 
 
 module.exports = routes
