f94a51d15a5a27cb38c4cfae3c5bf29d74af91e2
Luan <luan10017@hotmail.com>
2021-01-17 16:15:28 -0300

    Completei onlyUsers e isAdmin

--- src/routes/admin/users.js
+++ src/routes/admin/users.js
@@ -8,7 +8,7 @@ const UserController = require('../../app/controllers/admin/UserController')
 const UserValidator = require('../../app/validators/user')
 const SessionValidator = require('../../app/validators/session')
 
-const { isAdmin } = require('../../app/middlewares/session')
+const { isAdmin, onlyUsers } = require('../../app/middlewares/session')
 
 
 // login/logout
@@ -23,14 +23,14 @@ routes.post('/users/forgot-password', SessionValidator.forgot, SessionController
 routes.post('/users/password-reset', SessionValidator.reset, SessionController.reset)
 
 // Rotas de perfil de um usuário logado
-routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
-//routes.put('/admin/profile', ProfileController.put)// Editar o usuário logado
+routes.get('/profile', onlyUsers, ProfileController.index) // Mostrar o formulário com dados do usuário logado
+routes.put('/profile', UserValidator.update, ProfileController.update)// Editar o usuário logado
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-routes.get('/users', UserController.list) //Mostrar a lista de usuários cadastrados
-routes.get('/users/register', UserController.registerForm) //Mostrar a lista de usuários cadastrados
-routes.post('/users/register', UserValidator.post, UserController.post) //Cadastrar um usuário
-routes.get('/users/edit', UserController.edit) //Mostrar a lista de usuários cadastradoslist
+routes.get('/users', isAdmin, UserController.list) //Mostrar a lista de usuários cadastrados
+routes.get('/users/register', isAdmin, UserController.registerForm) //Mostrar a lista de usuários cadastrados
+routes.post('/users/register', isAdmin, UserValidator.post, UserController.post) //Cadastrar um usuário
+routes.get('/users/edit', isAdmin, UserController.edit) //Mostrar a lista de usuários cadastradoslist
 
 //routes.put('/admin/users', UserController.put) // Editar um usuário
 routes.delete('/users', isAdmin, UserController.delete) // Deletar um usuário
