db99e44f629fdb8e7b7ee19b9357bce25412f1c1
Luan <luan10017@hotmail.com>
2021-01-17 16:09:07 -0300

    Completei profile e adicionei metodo update

--- src/app/controllers/admin/ProfileController.js
+++ src/app/controllers/admin/ProfileController.js
@@ -1,5 +1,32 @@
+const { update } = require("../../model/chef")
+const User = require("../../model/User")
+
 module.exports = {
-    index(req, res) {
-        return res.render("admin/user/index")
+    async index(req, res) {
+        const id = req.session.userId
+        const user = await User.findOne({ where: {id} })
+        
+        return res.render("admin/user/index", {user})
+    },
+    async update(req, res) {
+        const {name, email} = req.body
+        const id = req.session.userId
+        
+        try {
+            await User.update(id, {
+                name,
+                email
+            })
+            return res.render("admin/user/index", {
+                user: req.body,
+                success: "Dados atualizados com sucesso!"
+            })
+        }catch(err) {
+            console.error(err)
+            return res.render("admin/user/index", {
+                user: req.body,
+                error: "Erro inesperado, tente novamente"
+            })
+        }
     }
 }
