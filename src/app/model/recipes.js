db89f516fa9465dbb450fef965c6c8f24a86f555
Luan <luan10017@hotmail.com>
2021-01-21 14:34:02 -0300

    Adicionei findRecipesByUser

--- src/app/model/recipes.js
+++ src/app/model/recipes.js
@@ -115,6 +115,8 @@ module.exports = {
     },
     files(id) {
         return db.query(`SELECT * FROM files WHERE id = $1`, [id])
+    },
+    findRecipesByUser(id) {
+        return db.query(`SELECT * FROM recipes WHERE user_id = $1`, [id])
     }
-    
 }
