890d721508b076f7f89998692a38c1ff517b9389
Luan <luan10017@hotmail.com>
2021-01-21 14:35:19 -0300

    Coloquei bloqueio nas rotas dos chefs que são restritas a Admin

--- src/routes/admin/chefs.js
+++ src/routes/admin/chefs.js
@@ -3,12 +3,14 @@ const routes = express.Router()
 const chefsAdminController = require('../../app/controllers/admin/chefsController')
 const multer = require('../../app/middlewares/multer')
 
+const { isAdmin } = require('../../app/middlewares/session')
+
 routes.get('/', chefsAdminController.chefs)
-routes.get("/create", chefsAdminController.create)
+routes.get("/create", isAdmin, chefsAdminController.create)
 routes.get('/:index', chefsAdminController.details) 
-routes.post("/", multer.array("photos",1), chefsAdminController.post) 
-routes.get('/:index/edit', chefsAdminController.edit)
-routes.put("/", multer.array("photos",1), chefsAdminController.put); 
-routes.delete("/", chefsAdminController.delete); 
+routes.post("/", isAdmin, multer.array("photos",1), chefsAdminController.post) 
+routes.get('/:index/edit', isAdmin, chefsAdminController.edit)
+routes.put("/", isAdmin, multer.array("photos",1), chefsAdminController.put); 
+routes.delete("/", isAdmin, chefsAdminController.delete); 
 
 module.exports = routes
